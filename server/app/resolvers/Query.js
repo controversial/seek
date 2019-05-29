76144dd016a4e9b3e7def500da88676ffc0ad354
Luke Taylor <luke@deentaylor.com>
2019-05-29 09:40:07 -0400

    Add basic graphql API for fetching students

--- server/app/resolvers/Query.js
+++ server/app/resolvers/Query.js
@@ -1,1 +1,16 @@
-module.exports = {};
+module.exports = {
+  Query: {
+    student(root, args, context) {
+      return context.prisma.student({ username: args.username });
+    },
+  },
+
+  Student: {
+    schedule(root, args, context) { return context.prisma.student({ id: root.id }).schedule(); }
+  },
+
+  MasterScheduleEntry: {
+    class(root, args, context) { return context.prisma.masterScheduleEntry({ id: root.id }).class(); },
+    teacher(root, args, context) { return context.prisma.masterScheduleEntry({ id: root.id }).teacher(); },
+  },
+};
