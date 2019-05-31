6ddd0c471d35f103c6917a587c987152de503bfd
Luke Taylor <luke@deentaylor.com>
2019-05-31 09:05:33 -0400

    Add method to query a single teachers’s students

--- server/app/resolvers/Query.js
+++ server/app/resolvers/Query.js
@@ -3,6 +3,19 @@ module.exports = {
     student(root, args, context) {
       return context.prisma.student({ username: args.username });
     },
+
+    async students(root, args, context) {
+      const teacherClasses = await context.prisma
+        .user({ username: args.teacher })
+        .classes();
+      const teacherScheduleIds = teacherClasses.map(c => c.id);
+
+      return context.prisma.students({
+        where: {
+          schedule_some: { id_in: teacherScheduleIds },
+        },
+      });
+    },
   },
 
   Student: {
