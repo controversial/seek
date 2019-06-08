d691b2f2912515e2fa86049a2639de1b9d1f16d8
Luke Taylor <luke@deentaylor.com>
2019-06-08 14:08:13 -0400

    Allow querying a teacher's schedule for a given period

--- server/app/resolvers/Query.js
+++ server/app/resolvers/Query.js
@@ -19,6 +19,14 @@ module.exports = {
         : undefined;
       return context.prisma.students(query);
     },
+
+    async class(root, args, context) {
+      const classes = await context.prisma.user({ username: args.teacher }).classes();
+      const periodNum = args.period[0];
+      const letterDay = args.period[1];
+      return classes.find(c => parseInt(c.periodNum) === parseInt(periodNum)
+        && c.letterDay.toLowerCase() === letterDay.toLowerCase());
+    }
   },
 
   Student: {
