f1f247bbc0e409731dc33f07a312670c236e06d2
Luke Taylor <luke@deentaylor.com>
2019-06-08 20:37:00 -0400

    implement sidebar

--- app/src/components/sidebar/sidebar.vue
+++ app/src/components/sidebar/sidebar.vue
@@ -1,5 +1,35 @@
 <template>
-  <div class="sidebar">Sidebar</div>
+  <div class="sidebar">
+    <router-link to="/">
+      <icon-bar-chart></icon-bar-chart>
+      Dashboard
+    </router-link>
+
+    <router-link to="/students">
+      <icon-users></icon-users>
+      Students
+    </router-link>
+
+    <router-link to="/classes">
+      <icon-calendar></icon-calendar>
+      Classes
+    </router-link>
+
+    <router-link to="/notifications">
+      <icon-bell></icon-bell>
+      Notifications
+    </router-link>
+
+    <router-link to="/messages">
+      <icon-message-square></icon-message-square>
+      Messages
+    </router-link>
+
+    <router-link to="/settings">
+      <icon-settings></icon-settings>
+      Settings
+    </router-link>
+  </div>
 </template>
 
 <script src="./script.js"></script>
