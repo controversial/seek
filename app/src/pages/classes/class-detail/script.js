cbb4581499fb507c33ff5824d3bceee73817008b
Luke Taylor <luke@deentaylor.com>
2019-06-09 20:44:28 -0400

    Fetch student events for time period of class being viewed

--- app/src/pages/classes/class-detail/script.js
+++ app/src/pages/classes/class-detail/script.js
@@ -4,6 +4,8 @@ import { ordinal } from '../../../utils';
 
 const statuses = {};
 
+function todayAtTime(time) { return new Date(`${new Date().toLocaleDateString()} ${time}`); }
+
 export default {
   data: () => ({
     data: {},
@@ -32,13 +34,25 @@ export default {
     },
   },
 
+
+  // DATA FETCHING
+
+
   apollo: {
     class: {
       query: gql`
         query FetchQuery($teacher: String!, $period: String!) {
           class(teacher: $teacher, period: $period) {
             class { name }
-            students { name username gradeLevel location { name } }
+            startTime
+            endTime
+            students {
+              id
+              name
+              username
+              gradeLevel
+              location { name }
+            }
           }
         }
       `,
@@ -48,7 +62,44 @@ export default {
           period: this.$route.params.period,
         };
       },
-      update(data) { this.data = data.class; },
+      update(data) {
+        this.data = data.class;
+        this.data.startTime = todayAtTime(this.data.startTime);
+        this.data.endTime = todayAtTime(this.data.endTime);
+        this.$apollo.queries.attendance.skip = false;
+      },
+    },
+
+    // Fetch relevant Events for each student once the first query reveals the relevant timeframe
+    attendance: {
+      skip: true,
+      query: gql`
+        query AttendanceQuery($students: [ID!]!, $from: String!, $to: String!) {
+          students(ids: $students) {
+            id
+            events(from: $from, to: $to) {
+              type
+              timestamp
+              location { name }
+            }
+          }
+        },
+      `,
+      variables() {
+        return {
+          students: this.data.students.map(s => s.id),
+          //                                             Ten minutes before
+          from: new Date(this.data.startTime.getTime() - (10 * 60000)).toISOString(),
+          to: this.data.endTime.toISOString(),
+        };
+      },
+      update(data) {
+        // Combine old students data with new students data
+        this.data.students = this.data.students.map(s => (({
+          ...s,
+          ...data.students.find(s2 => s2.id === s.id),
+        })));
+      },
     },
   },
 };
